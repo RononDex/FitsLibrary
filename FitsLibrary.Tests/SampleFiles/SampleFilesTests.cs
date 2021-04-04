e58ca7372fa5041dc8e91d025a241befcd0bac32
RononDex <tinoheuberger@protonmail.com>
2021-04-04 16:03:12 +0200

    Setup benchmarking / profiling of code

--- FitsLibrary.Tests/SampleFiles/SampleFilesTests.cs
+++ FitsLibrary.Tests/SampleFiles/SampleFilesTests.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
 using NUnit.Framework;
 
 namespace FitsLibrary.Tests.SampleFiles
 {
+    [SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.NetCoreApp50, launchCount: 5, warmupCount: 5, targetCount: 5)]
+    [MemoryDiagnoser]
     public class SampleFilesTests
     {
         [Test]
+        [Benchmark]
         public async Task OpenFitsFile_WithFOCFile_ReadsFileAsync()
         {
             Console.WriteLine("Reading sample file");
             var startTime = DateTime.Now;
 
             var reader = new FitsDocumentReader();
-            var document = await reader.ReadAsync("/home/cobra/test.fits");
+            var document = await reader.ReadAsync("SampleFiles/FOCx38i0101t_c0f.fits");
 
             var endTime = DateTime.Now;
             Console.WriteLine($"Sample file read in {(endTime - startTime).TotalSeconds}s");
