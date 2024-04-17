cb1fd4fd9df8d56b415ed5ac734255b5bf919f47
RononDex <tinoheuberger@protonmail.com>
2024-04-17 16:52:15 +0200

    Refactor code to be more easily readable

--- FitsLibrary/Validation/Header/MandatoryHeaderEntriesValidator.cs
+++ FitsLibrary/Validation/Header/MandatoryHeaderEntriesValidator.cs
@@ -13,17 +13,7 @@ namespace FitsLibrary.Validation.Header
         {
             return Task.Run(() =>
             {
-                var hasMandatoryFields = objToValidate.Entries.Count >= MandatoryFields.Length;
-                if (hasMandatoryFields)
-                {
-                    for (int i = 0; i < MandatoryFields.Length; i++)
-                    {
-                        if (objToValidate.Entries[i].Key != MandatoryFields[i])
-                        {
-                            hasMandatoryFields = false;
-                        }
-                    }
-                }
+                var hasMandatoryFields = ValidateMandatoryFields(objToValidate);
 
                 if (!hasMandatoryFields)
                 {
@@ -32,9 +22,7 @@ namespace FitsLibrary.Validation.Header
                         validationFailureMessage: "The FITS header is missing required fields (or they are in the wrong location).");
                 }
 
-                var numberOfAxisObject = objToValidate.Entries
-                    .SingleOrDefault(entry => string.Equals(entry.Key, "NAXIS", StringComparison.Ordinal))?.Value;
-                var numberOfAxis = numberOfAxisObject as int? ?? numberOfAxisObject as long?;
+                var numberOfAxis = GetNumberOfAxis(objToValidate);
 
                 if (numberOfAxis == null)
                 {
@@ -42,14 +30,7 @@ namespace FitsLibrary.Validation.Header
                         validationSuccessful: false,
                         validationFailureMessage: "The FITS header contains the field 'NAXIS' but it is not of type integer");
                 }
-                var hasAllRequiredNAXISKeywords = objToValidate.Entries.Count >= MandatoryFields.Length + numberOfAxis;
-                for (var i = 0; i < numberOfAxis; i++)
-                {
-                    if (objToValidate.Entries[i + MandatoryFields.Length].Key != $"NAXIS{(i + 1).ToString(CultureInfo.InvariantCulture)}")
-                    {
-                        hasAllRequiredNAXISKeywords = false;
-                    }
-                }
+                var hasAllRequiredNAXISKeywords = ValidateAllAxisDefinitionsPresent(objToValidate, numberOfAxis);
 
                 return hasAllRequiredNAXISKeywords
                     ? new ValidationResult(
@@ -59,6 +40,45 @@ namespace FitsLibrary.Validation.Header
                             validationSuccessful: false,
                             validationFailureMessage: "The FITS header does not contain required fields.");
             });
+        }
+
+        private static bool ValidateAllAxisDefinitionsPresent(DocumentParts.Header objToValidate, long? numberOfAxis)
+        {
+            var hasAllRequiredNAXISKeywords = objToValidate.Entries.Count >= MandatoryFields.Length + numberOfAxis;
+            for (var i = 0; i < numberOfAxis; i++)
+            {
+                if (objToValidate.Entries[i + MandatoryFields.Length].Key != $"NAXIS{(i + 1).ToString(CultureInfo.InvariantCulture)}")
+                {
+                    hasAllRequiredNAXISKeywords = false;
+                }
+            }
+
+            return hasAllRequiredNAXISKeywords;
+        }
+
+        private static long? GetNumberOfAxis(DocumentParts.Header objToValidate)
+        {
+            var numberOfAxisObject = objToValidate.Entries
+                .SingleOrDefault(entry => string.Equals(entry.Key, "NAXIS", StringComparison.Ordinal))?.Value;
+            var numberOfAxis = numberOfAxisObject as int? ?? numberOfAxisObject as long?;
+            return numberOfAxis;
+        }
+
+        private static bool ValidateMandatoryFields(DocumentParts.Header objToValidate)
+        {
+            var hasMandatoryFields = objToValidate.Entries.Count >= MandatoryFields.Length;
+            if (hasMandatoryFields)
+            {
+                for (int i = 0; i < MandatoryFields.Length; i++)
+                {
+                    if (objToValidate.Entries[i].Key != MandatoryFields[i])
+                    {
+                        hasMandatoryFields = false;
+                    }
+                }
+            }
+
+            return hasMandatoryFields;
         }
     }
 }
